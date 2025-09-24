e83b719e65d3b0456513a9177dfdb147aed7e47c
HugoxDP <sasha.maidanika@gmail.com>
2025-09-24 23:33:29 +0300

    feat(input): implement player input service for mouse interactions
    - Add IPlayerInputService interface for mouse click events
    - Create PlayerInputService class to handle mouse input using Unity's new Input System
    - Implement reactive observables for left and right mouse clicks
    - Integrate input service with unit movement commands in TestCommander

---
+++ Assets/_Project/Scripts/Architecture/InputSystems/PlayerInputService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using R3;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using static InputSystem_Actions;
+
+namespace Architecture.InputSystems
+{
+    public interface IPlayerInputService
+    {
+        public Observable<Vector3> OnMouseClick { get; }
+        public Observable<Vector3> OnMouseRightClick { get; }
+    }
+    
+    public class PlayerInputService: IPlayerActions, IPlayerInputService, IDisposable
+    {
+        public Observable<Vector3> OnMouseClick => _onMouseClick;
+        public Observable<Vector3> OnMouseRightClick => _onMouseRightClick;
+        
+        private Subject<Vector3> _onMouseClick = new Subject<Vector3>();
+        private Subject<Vector3> _onMouseRightClick = new Subject<Vector3>();
+
+        private readonly InputSystem_Actions _actions;
+        
+        private Vector3 MousePosition => Input.mousePosition;
+        
+        public PlayerInputService(InputSystem_Actions actions)
+        {
+            _actions = actions;
+            _actions.Player.Enable();
+            
+            _actions.Player.Click.performed += OnClick;
+            _actions.Player.RightClick.performed += OnRightClick;
+        }
+        
+        public void OnClick(InputAction.CallbackContext context)
+        {
+            _onMouseClick?.OnNext(MousePosition);
+        }
+
+        public void OnRightClick(InputAction.CallbackContext context)
+        {
+            _onMouseRightClick?.OnNext(MousePosition);
+        }
+
+        public void Dispose()
+        {
+            _actions.Player.Click.performed -= OnClick;
+            _actions.Player.RightClick.performed -= OnRightClick;
+            
+            _onMouseClick?.Dispose();
+            _onMouseRightClick?.Dispose();
+            _actions.Player.Disable();
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/Architecture/TestCommander.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Architecture.InputSystems;
+using Architecture.Units;
+using R3;
+using Reflex.Attributes;
+using UnityEngine;
+
+namespace Architecture
+{
+    public class TestCommander : MonoBehaviour
+    {
+        [SerializeField] private BaseUnit _unit;
+        [SerializeField] private LayerMask _clickableLayerMask;
+        
+        private RaycastHit _hitInfo;
+        
+        [Inject] private IPlayerInputService _playerInputService;
+        [Inject] private UnityEngine.Camera _camera;
+        
+        private CompositeDisposable _disposables = new CompositeDisposable();
+        private void Start()
+        {
+            if (_unit == null) throw new ArgumentNullException(nameof(_unit), "Unit cannot be null");
+            if (_playerInputService == null) throw new ArgumentNullException(nameof(_playerInputService), "PlayerInputService cannot be null");
+            
+            _playerInputService.OnMouseClick.Subscribe(position =>
+            {
+                var ray = _camera.ScreenPointToRay(position);
+                if (!Physics.Raycast(ray.origin, ray.direction, out _hitInfo, 100, _clickableLayerMask)) return;
+                
+                Debug.Log($"Moving unit to {_hitInfo.point}");
+                _unit.MoveTo(_hitInfo.point);
+            }).AddTo(_disposables);
+            
+            _playerInputService.OnMouseRightClick.Subscribe(position =>
+            {
+                _unit.Stop();
+                _hitInfo = new RaycastHit();
+            }).AddTo(_disposables);
+        }
+
+        private void OnDestroy()
+        {
+            _disposables.Dispose();
+            _disposables = null;
+        }
+
+        private void OnDrawGizmos()
+        {
+            if (UnityEditor.EditorApplication.isPlaying)
+            {
+                Gizmos.color = _hitInfo.collider != null ? Color.red : Color.green;
+                Gizmos.DrawLine(_hitInfo.point, _hitInfo.point + Vector3.up * 10);
+            }
+        }
+    }
+}
